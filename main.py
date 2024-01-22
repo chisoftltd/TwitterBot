dce48a53fbf7f86338e076de64e283cb81ced51d
chisoftltd <chisoftltd@gmail.com>
2024-01-22 23:50:15 +0000

    Update Bot to the new Twitter (X) Html layer out

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class InternetSpeedTwitterBot:
         go_button.click()
 
         time.sleep(3)
-        reject_button = self.driver.find_element(by=By.ID, value='#onetrust-accept-btn-handler')
+        reject_button = self.driver.find_element(by=By.XPATH, value='/html/body/div[5]/div[2]/div/div/div[2]/div/div/button[1]')
         reject_button.click()
 
         time.sleep(60)
@@ -47,20 +47,21 @@ class InternetSpeedTwitterBot:
             '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
         self.down = self.driver.find_element(by=By.XPATH, value=
             '/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
+        print(self.up)
+        print(self.down)
 
     def tweet_at_provider(self):
         self.driver.get("https://twitter.com/login")
 
         time.sleep(2)
         email = self.driver.find_element(by=By.XPATH, value=
-            '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')
-        time.sleep(3)
-        next = self.driver.find_element(by=By.XPATH, value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div/span')
-        time.sleep(3)
-        password = self.driver.find_element(by=By.XPATH, value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
-
+            '/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')
         email.send_keys(EMAIL)
-        next.send_keys(Keys.ENTER)
+        time.sleep(3)
+        next = self.driver.find_element(by=By.XPATH, value='/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div/span/span')
+        next.click()
+        time.sleep(3)
+        password = self.driver.find_element(by=By.XPATH, value='/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
         password.send_keys(PASSWORD)
         time.sleep(2)
         password.send_keys(Keys.ENTER)
@@ -73,8 +74,7 @@ class InternetSpeedTwitterBot:
         tweet_compose.send_keys(tweet)
         time.sleep(3)
 
-        tweet_button = self.driver.find_element_by_xpath(
-            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+        tweet_button = self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/div/div/div[2]/header/div/div/div/div[1]/div[3]/a/div/span/div/div/span/span')
         tweet_button.click()
 
         time.sleep(2)
