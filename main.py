4d612201cdb82563b172cbb244f6b4b8ebf1d78c
chisoftltd <chisoftltd@gmail.com>
2024-01-22 18:39:48 +0000

    Update Bot to the new Twitter (X) Html layer out

--- main.py
+++ main.py
@@ -35,25 +35,33 @@ class InternetSpeedTwitterBot:
         # accept_button.click()
 
         time.sleep(3)
-        go_button = self.driver.find_element_by_css_selector(".start-button a")
+        go_button = self.driver.find_element(by=By.CSS_SELECTOR, value=".start-button a")
         go_button.click()
 
+        time.sleep(3)
+        reject_button = self.driver.find_element(by=By.ID, value='#onetrust-accept-btn-handler')
+        reject_button.click()
+
         time.sleep(60)
-        self.up = self.driver.find_element_by_xpath(
+        self.up = self.driver.find_element(by=By.XPATH, value=
             '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
-        self.down = self.driver.find_element_by_xpath(
-            '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+        self.down = self.driver.find_element(by=By.XPATH, value=
+            '/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
 
     def tweet_at_provider(self):
         self.driver.get("https://twitter.com/login")
 
         time.sleep(2)
-        email = self.driver.find_element_by_xpath(
-            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
-        password = self.driver.find_element_by_xpath(
-            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
+        email = self.driver.find_element(by=By.XPATH, value=
+            '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')
+        time.sleep(3)
+        next = self.driver.find_element(by=By.XPATH, value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div/span')
+        time.sleep(3)
+        password = self.driver.find_element(by=By.XPATH, value=
+            '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
 
         email.send_keys(EMAIL)
+        next.send_keys(Keys.ENTER)
         password.send_keys(PASSWORD)
         time.sleep(2)
         password.send_keys(Keys.ENTER)
